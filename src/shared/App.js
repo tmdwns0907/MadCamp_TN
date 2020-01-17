340ea45fdbe2e7841d8b7a20d301f8d642fecd0b
busankimchi <jacky185@kaist.ac.kr>
2020-01-17 21:20:09 +0900

    added router

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { fire } from 'shared/Firebase'
+import { Route } from 'react-router-dom';
+import { Home, About, Posts } from 'pages';
+import Menu from 'components/Menu';
+
+import { fire } from 'shared/Firebase';
 
 class App extends Component {
     constructor() {
         super();
         fire();
     }
-    
+
     render() {
+        /*
         return (
             <div>
-                Hello React-Router
+                <Menu />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route path="/about/:name" component={About}/>
+                <Route path="/posts" component={Posts}/>
             </div>
         );
+        */
     }
 }
 
