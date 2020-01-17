857b1243fca4ebd7b62559674e3dfc9c26d3d00d
오승준 <tmdwns0907@naver.com>
2020-01-18 02:25:53 +0900

    size

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,12 @@ import './Form.css';
 
 const Form = ({value, onChange, onCreate, onKeyPress}) => {
   return (
-    <div className="form">
-      <input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
+    //<div className="form">
+      //input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
       <div className="create-button" onClick={onCreate}>
         추가
       </div>
-    </div>
+    //</div>
   );
 };
 
